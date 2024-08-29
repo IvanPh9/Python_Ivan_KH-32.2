7c527cdb2a6cd4cb65d79116f4288203bbaafd71
IvanPh9 <igruin12@gmail.com>
2024-08-29 17:35:51 +0300

    28.08

--- lab3_1/lab3_1.py
+++ lab3_1/lab3_1.py
@@ -1,10 +1,9 @@
 string = input("Введіть слово: ")
-string_1 = ""
 
 while not string.isalpha() or " " in string or len(string) < 3:
     print("Лише одне слово без цифр з кількістю літер більшу за 3:")
     string = input("Спробуйте ще раз: ")
-print("Кількість літер у слові:",len(string))
-for i in range(2, len(string), 3):
-    string_1 += string[i]
-print("Кожна третя буква слова: ",string_1)
+
+string_1 = string[2::3]
+
+print("Кожна третя буква слова:", string_1)
